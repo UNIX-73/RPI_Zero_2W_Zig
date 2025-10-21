421ede95e2402b984ac60f90d478ad4286fc80da
Unai <iborra.unai@gmail.com>
2025-10-21 16:42:36 +0200

    working build.zig

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const allocator = b.allocator;
     const target = b.resolveTargetQuery(.{
         .cpu_arch = .aarch64,
         .os_tag = .freestanding,
@@ -29,57 +28,19 @@ pub fn build(b: *std.Build) void {
         "irq",
         irq_module,
     );
+
     //  Drivers
-    const drivers_module = b.createModule(.{
-        .root_source_file = b.path(
-            "./src/drivers/drivers.zig",
-        ),
-        .target = target,
-    });
+    const drivers_module = build_module(b, target, "drivers") catch return;
 
     irq_module.addImport("drivers", drivers_module);
     elf.root_module.addImport("drivers", drivers_module);
     //  Boot
-    const boot_module = b.createModule(.{
-        .root_source_file = b.path(
-            "./src/boot/boot.zig",
-        ),
-        .target = target,
-    });
+    const boot_module = build_module(b, target, "boot") catch return;
 
     elf.root_module.addImport("boot", boot_module);
 
     // -- ASM --
-    const src_dir = std.fs.cwd().openDir(
-        "./src",
-        .{ .iterate = true },
-    ) catch return;
-    var it = src_dir.walk(allocator) catch return;
-    defer it.deinit();
-
-    while (it.next() catch return) |entry| {
-        if (entry.kind == .file and
-            (std.mem.endsWith(u8, entry.path, ".S") or std.mem.endsWith(u8, entry.path, ".s")))
-        {
-            elf.root_module.addAssemblyFile(
-                b.path(
-                    b.pathJoin(&.{
-                        "./src/",
-                        entry.path,
-                    }),
-                ),
-            );
-
-            drivers_module.addAssemblyFile(
-                b.path(
-                    b.pathJoin(&.{
-                        "./src/",
-                        entry.path,
-                    }),
-                ),
-            );
-        }
-    }
+  
 
     elf.setLinkerScript(b.path("./linker.ld"));
 
@@ -94,16 +55,53 @@ pub fn build(b: *std.Build) void {
     b.default_step.dependOn(&copy_bin.step);
 }
 
-fn build_module(b: *std.Build, path: []const u8) *std.Build.Module {
+fn build_module(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    path: []const u8,
+) !*std.Build.Module {
+    var buf: [4096]u8 = undefined;
+
     const module = b.createModule(.{
         .root_source_file = b.path(
-
-            b.pathJoin(paths: []const []const u8)
-            "./src/drivers/drivers.zig",
+            try std.fmt.bufPrint(
+                &buf,
+                "./src/{s}/{s}.zig",
+                .{ path, path },
+            ),
         ),
         .target = target,
     });
 
-    irq_module.addImport("drivers", drivers_module);
-    elf.root_module.addImport("drivers", drivers_module);
+    var asm_buf: [4096]u8 = undefined;
+    const asm_dir_u8 = try std.fmt.bufPrint(
+        &asm_buf,
+        "./src/{s}",
+        .{path},
+    );
+
+    const asm_dir = try std.fs.cwd().openDir(
+        asm_dir_u8,
+        .{ .iterate = true },
+    );
+
+    var it = try asm_dir.walk(b.allocator);
+    defer it.deinit();
+
+    while (try it.next()) |entry| {
+        if (entry.kind == .file and
+            (std.mem.endsWith(u8, entry.path, ".S") or std.mem.endsWith(u8, entry.path, ".s")))
+        {
+            module.addAssemblyFile(
+                b.path(
+                    b.pathJoin(&.{
+                        asm_dir_u8,
+                        entry.path,
+                    }),
+                ),
+            );
+        }
+    }
+
+    return module;
 }
